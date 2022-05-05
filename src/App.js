962d28d8bd52c895608d26144ea4c1430804c62c
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-05 11:40:32 +0600

    install react-router and set rout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Items from './components/Items/Items';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello faysal</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='items' element={<Items></Items>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+        {/* <Route></Route> */}
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/items">Items</Link>
+        </div>
+    );
+};
+
+export default Header;
